cf5f0474394872610049bb18ef99061227ad5c1d
Srimathi <srimathi@integra.co.in>
2023-10-01 18:50:33 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import logo from "./images/logo.jpg";
+import React, { useState } from "react";
+import "./App.css";
+import {  Route, NavLink,Routes } from 'react-router-dom';
+import Home from './Home';
+
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+        <img src={logo} alt="QUINTESSENCE SEARCH LABS" />
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <Hamburger />
+        </div>
+        <div className={`nav-elements  ${showNavbar && "active"}`}>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About us</NavLink>
+            </li>
+            <li>
+              <NavLink to="/services">Services</NavLink>
+            </li>
+            <li>
+              <NavLink to="/careers">Careers</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact us</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+const Careers = () => <h2>You are in the Blogs</h2>;
+const Services = () => <h2>You are in the Projects</h2>;
+const About = () => <h2>You are in the About</h2>;
+const Contact = () => <h2>You are in the Contact</h2>;
+
+function App () {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/careers" component={Careers} />
+        <Route path="/services" component={Services} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Routes>
+    </>
+  );
+};
+
+const Hamburger = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="52"
+    height="24"
+    viewBox="0 0 52 24"
+  >
+    <g id="Group_9" data-name="Group 9" transform="translate(-294 -47)">
+      <rect
+        id="Rectangle_3"
+        data-name="Rectangle 3"
+        width="42"
+        height="4"
+        rx="2"
+        transform="translate(304 47)"
+        fill="#574c4c"
+      />
+      <rect
+        id="Rectangle_5"
+        data-name="Rectangle 5"
+        width="42"
+        height="4"
+        rx="2"
+        transform="translate(304 67)"
+        fill="#574c4c"
+      />
+      <rect
+        id="Rectangle_4"
+        data-name="Rectangle 4"
+        width="52"
+        height="4"
+        rx="2"
+        transform="translate(294 57)"
+        fill="#574c4c"
+      />
+    </g>
+  </svg>
+);
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import "./Home.css";
+import SimpleImageSlider from "react-simple-image-slider";
+import HR3 from "./images/HR-3.jpg";
+import HR1 from "./images/HR-1.jpg";
+import leadership from "./images/leadership.jpg";
+import about from "./images/about-us.jpg";
+import border from "./images/m-1.png";
+import Consumer from "./images/Consumer.jpg";
+import Diversity from "./images/Diversity.jpg";
+import Financial from "./images/Financial.jpg";
+import Technology from "./images/Technology.jpg";
+import Investment from "./images/Investment.jpg";
+import Management from "./images/Management.jpg";
+import Careers from "./images/Careers.jpg";
+
+function Home() {
+
+    const sliderImages = [
+        {
+            url: HR3,
+        },
+        {
+            url: HR1,
+        },
+        {
+            url: leadership,
+        },
+    ];
+
+    return (
+        <div className="content-container">
+            <div className="item">
+                <SimpleImageSlider
+                    width={"100%"}
+                    height={"100%"}
+                    autoPlay={true}
+                    autoPlayDelay={3}
+                    images={sliderImages}
+                    showNavs={true}
+                />
+            </div>
+            <div className="item" id="about">
+                <div className="leftalgin">
+                    <img src={about} width={500} height={410} alt="About us" />
+                </div>
+                <div className="about-content">
+                    <div className="about-title slideInDown">
+                        <h2>About Quintessence Search Labs</h2>
+                    </div>
+                    <div>
+                        <img src={border} />
+                    </div>
+                    <div>
+                        <p>
+                            <span>
+                                Quintessence Search labs is a next generation Technological Service Provider for all business and also offers both strategic and operational solution. Exuberant team of HR professionals navigate our client, to enhance their business through a customized digital process. We help our clients to achieve next level by making their business go-digital.
+                            </span>
+                        </p>
+                        <p>
+                            <span>
+                                As a Human Resources team, we strive to create a workplace where our employees feel respected, valued, and appreciated. We also aim to promote a culture of open communication, collaboration, and engagement. We are committed to promoting a safe and healthy work environment, and to fostering an atmosphere of trust, integrity, and respect.
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            {/* <div className="statics-data item">
+                <p>welccome</p>
+            </div> */}
+            <div className="area-content">
+                <div className="about-title slideInDown">
+                    <h2>Area's Expertise</h2>
+                </div>
+                <div id="border-img">
+                    <img src={border} />
+                </div>
+                <div className="areas-imgs">
+                    <div>
+                        <img src={Consumer} width="350" height="250"/>
+                    </div>
+                    <div>
+                        <img src={Investment} width="350" height="250"/>
+                    </div>
+                    <div>
+                        <img src={Diversity} width="350" height="250"/>
+                    </div>
+                    <div>
+                        <img src={Financial} width="350" height="250"/>
+                    </div>
+                    <div>
+                        <img src={Technology} width="350" height="250"/>
+                    </div>
+                    <div>
+                        <img src={Management} width="350" height="250"/>
+                    </div>
+                </div>
+            </div>
+            <div className="Careers">
+                <div className="about-title slideInDown">
+                    <h2>Careers</h2>
+                </div>
+                <div id="border-img">
+                    <img src={border} />
+                </div>
+                <div>
+                    <img src={Careers} />
+                </div>
+                <div>
+                    <p  id="mail-text">Interested to join us? Why wait, email your profile to <a href="mailto:hr@quintessencesearchlabs.in">hr@quintessencesearchlabs.in</a></p>
+                </div>
+            </div>
+            <div className="Contact-area">
+                <p>We are here to answer your questions 24/7, NEED A CONSULTATION?</p>
+            </div>
+            <div className="Contact-details">
+                <p>We are here to answer your questions 24/7, NEED A CONSULTATION?</p>
+            </div>
+            <div className="Copyrights">
+                <p><i className="fa fa-copyright"></i> 2023 <a>Quintessence Search Labs</a></p>
+            </div>
+        </div>
+    );
+}
+
+
+export default Home;
